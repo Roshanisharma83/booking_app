73a22d7c33c98c8d5e1ef702de0aba3bda74d3cb
roshani <rsg.bhusal@gmail.com>
2024-02-27 11:07:51 +0545

    maillist

--- client/src/components/mail/page.js
+++ client/src/components/mail/page.js
@@ -4,7 +4,7 @@ import styles from "./style.module.css"
 const Mail = () => {
   return (
     <div className={styles.mail}>
-      <h1 className={styles.mailTitle}>Save time, save money!</h1>
+      <h1 className={styles.mailTitle}>Save time, save money !</h1>
       <span className={styles.mailDesc}>Sign up and we'll send the best deals to you</span>
       <div className={styles.mailInputContainer}>
         <input type="text" placeholder="Your Email" />
